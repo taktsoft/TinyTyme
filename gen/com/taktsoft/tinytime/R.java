f1612127a4c9c59a2effb7a01c44208b05205937
frankm@cs.uni-bonn.de <mfrank@.(none)>
2011-07-08 17:05:06 +0200

    fetching task from server and creating neu tasks

--- gen/com/taktsoft/tinytime/R.java
+++ gen/com/taktsoft/tinytime/R.java
@@ -14,20 +14,24 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int editText1=0x7f050004;
-        public static final int tableLayout1=0x7f050000;
-        public static final int tableRow1=0x7f050001;
-        public static final int tableRow2=0x7f050003;
-        public static final int tableRow3=0x7f050005;
-        public static final int tableRow4=0x7f050006;
-        public static final int textView1=0x7f050002;
+        public static final int ScrollView01=0x7f060007;
+        public static final int btnCreate=0x7f060005;
+        public static final int btnJSON=0x7f060000;
+        public static final int dpDate=0x7f060002;
+        public static final int inputTaskName=0x7f060004;
+        public static final int linearLayout1=0x7f060001;
+        public static final int tpTime=0x7f060003;
+        public static final int txtData=0x7f060006;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
+    public static final class raw {
+        public static final int tasks=0x7f040000;
+    }
     public static final class string {
-        public static final int TinyTyme=0x7f040002;
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int TinyTyme=0x7f050002;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
     }
 }
