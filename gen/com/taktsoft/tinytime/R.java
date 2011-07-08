f507dac90ad53e90219569713ae20e27a0a1b574
Michael Berg <berg@taktsoft.com>
2011-07-08 13:41:58 +0200

    added first interface

--- gen/com/taktsoft/tinytime/R.java
+++ gen/com/taktsoft/tinytime/R.java
@@ -10,24 +10,36 @@ package com.taktsoft.tinytime;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int background=0x7f050000;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int icon_inapp=0x7f020001;
     }
     public static final class id {
-        public static final int editText1=0x7f050004;
-        public static final int tableLayout1=0x7f050000;
-        public static final int tableRow1=0x7f050001;
-        public static final int tableRow2=0x7f050003;
-        public static final int tableRow3=0x7f050005;
-        public static final int tableRow4=0x7f050006;
-        public static final int textView1=0x7f050002;
+        public static final int commitbtn=0x7f070005;
+        public static final int editText1=0x7f070002;
+        public static final int imageView1=0x7f070001;
+        public static final int linearLayout1=0x7f070000;
+        public static final int linearLayout4=0x7f070003;
+        public static final int progressBar1=0x7f070004;
+        public static final int scrollView1=0x7f070006;
+        public static final int settings=0x7f070007;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int TinyTyme=0x7f040002;
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int TinyTyme=0x7f060003;
+        public static final int app_name=0x7f060002;
+        public static final int commit_to_tyme=0x7f060005;
+        public static final int hello=0x7f060001;
+        public static final int settings_title=0x7f060004;
+        public static final int todo=0x7f060000;
+    }
+    public static final class xml {
+        public static final int menu=0x7f040000;
+        public static final int settings=0x7f040001;
     }
 }
--- src/com/taktsoft/tinytime/TinyTymeMain.java
+++ src/com/taktsoft/tinytime/TinyTymeMain.java
@@ -1,7 +1,11 @@
 package com.taktsoft.tinytime;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
 
 public class TinyTymeMain extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,23 @@ public class TinyTymeMain extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.xml.menu, menu);
+    	return true;
+    }
+    
+    @Override 
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch(item.getItemId()) {
+    	case R.id.settings:
+    		startActivity(new Intent(this, Settings.class));
+    		return true;
+    	}
+    	return false;
     }
 }
