502ebad676a6ab86dde4bfce4b08ac8b07b811ff
Michael Berg <berg@taktsoft.com>
2011-07-08 11:19:28 +0200

    initial commit

---
+++ src/com/taktsoft/tinytime/TinyTymeMain.java
@@ -0,0 +1,13 @@
+package com.taktsoft.tinytime;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TinyTymeMain extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
