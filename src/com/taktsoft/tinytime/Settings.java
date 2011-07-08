42cf7512611c4752af60b3e94cca6374dceb3a47
Michael Berg <berg@taktsoft.com>
2011-07-08 20:34:09 +0200

    works :)

--- src/com/taktsoft/tinytime/Settings.java
+++ src/com/taktsoft/tinytime/Settings.java
@@ -1,5 +1,8 @@
 package com.taktsoft.tinytime;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
@@ -34,14 +37,42 @@ public class Settings extends PreferenceActivity {
 		Log.i("scan dialog res.", "Ive got data for you!");
 		IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
 		if (scanResult != null && scanResult.getContents() != "") {
+			
+			
+			
+			
+			
 			String jsonResult = scanResult.getContents();
-			PreferenceManager.getDefaultSharedPreferences(this).edit().putString("servername", jsonResult).commit();
+			String servername = null;
+			String authtoken = null;
+			try{
+				JSONObject data = new JSONObject(jsonResult); 
+				servername = data.getString("host");
+				authtoken = data.getString("auth_token");
+				/*AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+			    alertDialog.setTitle("JSON Results:");
+			    alertDialog.setMessage("host: "+ data.getString("host") + ", auth: " + data.getString("auth_token"));
+			    alertDialog.setButton("OK", new DialogInterface.OnClickListener() { public void onClick(DialogInterface dialog, int which) { return; } }); 
+			    alertDialog.show();*/
+			} catch(Exception e) {
+				AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+			    alertDialog.setTitle("JSON exception");
+			    alertDialog.setMessage(e.getMessage());
+			    alertDialog.setButton("OK", new DialogInterface.OnClickListener() { public void onClick(DialogInterface dialog, int which) { return; } }); 
+			    alertDialog.show();
+			}
+			if(servername != null && authtoken != null){
+				PreferenceManager.getDefaultSharedPreferences(this).edit().putString("servername", servername).commit();
+				PreferenceManager.getDefaultSharedPreferences(this).edit().putString("authtoken", authtoken).commit();
+			}
+			
+			
 			Log.i("scan dialog res.", "RESULT IS: " + jsonResult);
-			AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+			/*AlertDialog alertDialog = new AlertDialog.Builder(this).create();
 		    alertDialog.setTitle("Received Input!");
 		    alertDialog.setMessage("Input was: " + jsonResult);
 		    alertDialog.setButton("OK", new DialogInterface.OnClickListener() { public void onClick(DialogInterface dialog, int which) { return; } }); 
-		    alertDialog.show();
+		    alertDialog.show();*/
 			
 		} else {
 			Log.i("scan dialog res.", "NO RESULT");
